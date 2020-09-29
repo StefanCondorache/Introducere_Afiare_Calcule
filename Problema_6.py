620619aca996de537267c59cf5ed5387cabc0621
StefanCondorache <70690185+StefanCondorache@users.noreply.github.com>
2020-09-29 15:29:44 +0300

    Add files via upload

---
+++ Problema_6.py
@@ -0,0 +1,7 @@
+# Doi copii au primit acelaşi număr de mere Introducând de la tastatură numărul de mere
+# primte, afişaţi câte mere are fiecare copil după ce primul copil mănâncă un măr şi dă
+# unul celuilalt copil. Exemplu : Date de intrare : 10 Date de ieşire : primul copil 8
+# mere al doilea copil 11 mere. 
+I_copil=int(input("Nuamrul de mere primte="))
+II_copil=I_copil
+print("primul copil",I_copil-2,"mere, al doilea copil",II_copil+1,"mere")
