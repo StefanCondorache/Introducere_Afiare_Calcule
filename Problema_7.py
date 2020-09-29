347449d4ea81169f13c9734cac7a5aa8118e4d89
StefanCondorache <70690185+StefanCondorache@users.noreply.github.com>
2020-09-29 15:38:24 +0300

    Add files via upload

---
+++ Problema_7.py
@@ -0,0 +1,9 @@
+# Maria vrea să verifice dacă greutatea şi înălţimea ei corespund vârstei pe care o are. Ea
+# a găsit într-o carte următoarele formule de calcul ale greutăţii şi înălţimii unui copil, v
+# fiind vârsta : greutate=2*v+8 (în kg), înălţime=5*v+80 (în cm). Realizaţi un program
+# care să citească vârsta unui copil şi să afişeze greutatea şi înălţimea ideală, folosind aceste formule.
+v=int(input("varsta este egal cu "))
+greutatea=2*v+8
+înaltimea=5*v+80
+print("greutatea ideala pentru varsta de",v,"ani este",greutatea,"kg")
+print("inaltimea ideala este de",înaltimea,"cm")
