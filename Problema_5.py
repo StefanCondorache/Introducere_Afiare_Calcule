2b9d84df2b4d61b3de8523c956e16f71f61f4773
StefanCondorache <70690185+StefanCondorache@users.noreply.github.com>
2020-09-29 15:23:05 +0300

    Update Problema_5.py

--- Problema_5.py
+++ Problema_5.py
@@ -1,2 +1,5 @@
+# Ion şi Vasile joacă următorul joc: Ion spune un număr iar Vasile trebuie să găsească
+# cinci numere consecutive, crescătoare, numărul din mijloc fiind cel ales de Ion.
+# Exemplu : Ion spune 10, Vasile spune 8 9 10 11 12. Ajutaţi-l pe Vasile să găsească răspunsul mai repede.
 Ion=int(input("Ion a spus numarul "))
 print("Ion spune",Ion,",Vasile spune",Ion-2,Ion-1,Ion,Ion+1,Ion+2)
