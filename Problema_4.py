88025c64b1f658486f6e45230dee8cdd5a9a368c
StefanCondorache <70690185+StefanCondorache@users.noreply.github.com>
2020-09-29 15:22:01 +0300

    Update Problema_4.py

--- Problema_4.py
+++ Problema_4.py
@@ -2,7 +2,7 @@
 # albe se citeşte de la tastatură. Câte globuleţe are brăduţul, ştiind că numărul de
 # globuleţe roşii este cu 3 mai mare decât numărul de globuleţe albe, iar globuleţele
 # albastre sunt cu 2 mai puţine decât totalul celor albe şi roşii. 
-#Exemplu: Date de intrare: 12 Date de ieşire: 52.
+# Exemplu: Date de intrare: 12 Date de ieşire: 52.
 gla=int(input("gla="))
 glr=3+gla
 glab=(gla+glr)-2
