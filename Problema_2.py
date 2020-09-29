fa102d16bfc2f91cc2b26356d6cfecd04cf88188
StefanCondorache <70690185+StefanCondorache@users.noreply.github.com>
2020-09-29 15:12:45 +0300

    Update Problema_2.py

--- Problema_2.py
+++ Problema_2.py
@@ -1,3 +1,6 @@
+# Într-o tabără numărul de băieţi este cu 10 mai mare decât cel al fetelor.
+# Dacă se citeşte de la tastatură numărul de fete, să se spună câţi elevi sunt în tabără. 
+# Exemplu: date de intrare: 50 date de ieşire: 110.
 f=int(input("f="))
 b=10+f
 print("in total",f+b, "de copii sunt in tabără")
