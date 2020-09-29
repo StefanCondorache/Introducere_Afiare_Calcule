05621d0e70968860d20f0d46eab22aa6b4d61055
StefanCondorache <70690185+StefanCondorache@users.noreply.github.com>
2020-09-29 16:19:22 +0300

    Add files via upload

---
+++ Problema_12.py
@@ -0,0 +1,7 @@
+# Într-o gospodărie sunt 4 găini. Introduceţi în calculator prin variabilele a, b, c, d
+# numărul de ouă pe care-l dă fiecare găină într-o zi. Afişaţi câte ouă se obţin într-o săptămână.
+a=int(input("a="))
+b=int(input("b="))
+c=int(input("c="))
+d=int(input("d="))
+print((a+b+c+d)*7,"ouă se obţin într-o săptămână.")
