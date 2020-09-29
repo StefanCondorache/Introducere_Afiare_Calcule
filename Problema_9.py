fc198ff795c4c9253a521cbb55c7539fd74f298a
StefanCondorache <70690185+StefanCondorache@users.noreply.github.com>
2020-09-29 16:08:14 +0300

    Add files via upload

---
+++ Problema_9.py
@@ -0,0 +1,7 @@
+# Date trei numere, să se calculeze toate sumele posibile de câte două numere. Afişarea
+# să cuprindă şi termenii sumei, nu numai valoarea ei. Exemplu: Date de intrare : 2 13 4 
+# Date de ieşire: 2+13=15 2+4=6 13+4=17.
+a=int(input("a="))
+b=int(input("b="))
+c=int(input("c="))
+print(a,"+",b,"=",a+b," ",a,"+",c,"=",a+c," ",b,"+",c,"=",b+c,sep="")
