0ae40bfc3e2342bd6c525f37f436190353c8dafc
StefanCondorache <70690185+StefanCondorache@users.noreply.github.com>
2020-09-29 15:14:29 +0300

    Update Problema_3.py

--- Problema_3.py
+++ Problema_3.py
@@ -1,3 +1,7 @@
+# Într-un autobuz care pleacă în excursie sunt 7 copii. 
+# De la încă două şcoli urcă alţi copii, numărul acestora citindu-se de la tastatura. 
+# Câţi copii au plecat în excursie?
+# Exemplu: Date de intrare: 15 20 Date de ieşire: 42 copii.
 total=7
 nr1=int(input("nr1="))
 nr2=int(input("nr2="))
